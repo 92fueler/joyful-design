f7b1459e944ee4f6797b5c0016a602c5078a1e54
Joyce Wu <jianjoycewu@gmail.com>
2020-12-10 01:25:10 -0800

    upgrade Button component

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,21 @@ const App: React.FC = () => {
   return (
     <>
       <h1>Welcome to Joyful Design World !</h1>
-      <Button> regular button</Button>
-      <Button disabled>disabled button</Button>
+      <Button btnType={ButtonType.Default}>default button</Button>
+      <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>
+        danger, small-size button
+      </Button>
+      <Button disabled btnType={ButtonType.Primary}>
+        disabled primary button
+      </Button>
       <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
         primary, large-size button
       </Button>
+
       <Button btnType={ButtonType.Link} href="http://www.google.com">
+        link button, linking to Google.com
+      </Button>
+      <Button btnType={ButtonType.Link} disabled href="http://www.google.com">
         link button, linking to Google.com
       </Button>
     </>
