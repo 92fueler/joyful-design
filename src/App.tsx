9fd18c6bd87cf72178bdebe93bbd6b9eddbaa307
Joyce Wu <jianjoycewu@gmail.com>
2020-12-10 00:40:46 -0800

    init Button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Button, { ButtonType, ButtonSize } from "./components/Button/button";
 
 const App: React.FC = () => {
   return (
     <>
       <h1>Welcome to Joyful Design World !</h1>
-      <a href="www.google.com">Search</a>
+      <Button> regular button</Button>
+      <Button disabled>disabled button</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+        primary, large-size button
+      </Button>
+      <Button btnType={ButtonType.Link} href="www.google.com">
+        link button, linking to Google.com
+      </Button>
     </>
   );
 };
