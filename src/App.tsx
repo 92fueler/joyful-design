03adfacada7ba459d5eb7178a9aa0f1609dd42d0
Joyce Wu <jianjoycewu@gmail.com>
2020-12-09 23:19:26 -0800

    prep done

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>Welcome to Joyful Design World !</h1>
+      <a href="www.google.com">Search</a>
+    </>
+  );
+};
+
+export default App;
