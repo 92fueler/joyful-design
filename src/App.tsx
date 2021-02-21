5d6e4f0766911236b2e1832fe72f8f4fb0e38a58
Joyce Wu <jianjoycewu@google.com>
2021-02-21 01:25:53 -0800

    Icon, Menu, Transition components init

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,53 @@
-import React from "react";
-import Button, { ButtonType, ButtonSize } from "./components/Button/button";
+import React from 'react'
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
+import Menu from './components/Menu/menu'
+import MenuItem from './components/Menu/menuItem'
+import SubMenu from './components/Menu/subMenu'
 
 const App: React.FC = () => {
   return (
-    <>
-      <h1>Welcome to Joyful Design World !</h1>
-      <Button btnType={ButtonType.Default}>default button</Button>
-      <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>
-        danger, small-size button
-      </Button>
-      <Button disabled btnType={ButtonType.Primary}>
-        disabled primary button
-      </Button>
-      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
-        primary, large-size button
-      </Button>
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Joyful Design World !</h1>
+        <div>
+          <h2>Menu components</h2>
+          <Menu defaultIndex={'0'}>
+            <MenuItem>cool link</MenuItem>
+            <MenuItem>cool link 1</MenuItem>
+            <MenuItem>cool link 2</MenuItem>
+            <SubMenu title="test">
+              <MenuItem>cool link</MenuItem>
+              <MenuItem>cool link 1</MenuItem>
+              <MenuItem>cool link 2</MenuItem>
+            </SubMenu>
+          </Menu>
+        </div>
+        <div>
+          <h2>Button components</h2>
+          <Button btnType={ButtonType.Default}>default button</Button>
+          <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>
+            danger, small-size button
+          </Button>
+          <Button disabled btnType={ButtonType.Primary}>
+            disabled primary button
+          </Button>
+          <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+            primary, large-size button
+          </Button>
+          <Button btnType={ButtonType.Link} href="http://www.google.com">
+            link button, linking to Google.com
+          </Button>
+          <Button
+            btnType={ButtonType.Link}
+            disabled
+            href="http://www.google.com"
+          >
+            link button, linking to Google.com
+          </Button>
+        </div>
+      </header>
+    </div>
+  )
+}
 
-      <Button btnType={ButtonType.Link} href="http://www.google.com">
-        link button, linking to Google.com
-      </Button>
-      <Button btnType={ButtonType.Link} disabled href="http://www.google.com">
-        link button, linking to Google.com
-      </Button>
-    </>
-  );
-};
-
-export default App;
+export default App
---
+++ src/components/Menu/menu.tsx
@@ -0,0 +1,88 @@
+import React, { FC, useState, createContext, CSSProperties } from 'react'
+import classNames from 'classnames'
+import { MenuItemProps } from './menuItem'
+
+type MenuMode = 'horizontal' | 'vertical'
+type SelectCallback = (selectedIndex: string) => void
+
+export interface MenuProps {
+  defaultIndex?: string // set up default active item
+  className?: string
+  mode?: MenuMode // menu mode: horizontal (top menu) and vertical (side menu)
+  style?: CSSProperties
+  onSelect?: SelectCallback // select and trigger callback func
+  defaultOpenSubMenus?: string[] // only works on the vertical mode
+}
+
+interface IMenuContext {
+  index: string
+  onSelect?: SelectCallback
+  mode?: MenuMode
+  defaultOpenSubMenus?: string[]
+}
+
+export const MenuContext = createContext<IMenuContext>({ index: '0' })
+
+export const Menu: FC<MenuProps> = (props) => {
+  const {
+    className,
+    mode,
+    style,
+    children,
+    defaultIndex,
+    onSelect,
+    defaultOpenSubMenus,
+  } = props
+
+  const [currentActive, setActive] = useState(defaultIndex)
+  const classes = classNames('joyful-menu', className, {
+    'menu-vertical': mode === 'vertical',
+    'menu-horizontal': mode !== 'vertical',
+  })
+
+  const handleClick = (index: string) => {
+    setActive(index)
+    if (onSelect) {
+      onSelect(index)
+    }
+  }
+
+  const passedContext: IMenuContext = {
+    index: currentActive ? currentActive : '0',
+    onSelect: handleClick,
+    mode,
+    defaultOpenSubMenus,
+  }
+
+  const renderChildren = () => {
+    return React.Children.map(children, (child, index) => {
+      const childElement = child as React.FunctionComponentElement<MenuItemProps>
+      const { displayName } = childElement.type
+      if (displayName === 'MenuItem' || displayName === 'SubMenu') {
+        return React.cloneElement(childElement, {
+          index: index.toString(),
+        })
+      } else {
+        console.error(
+          'Warning: Menu has a child which is not a MenuItem component'
+        )
+      }
+    })
+  }
+
+  return (
+    <ul className={classes} style={style} data-testid="test-menu">
+      <MenuContext.Provider value={passedContext}>
+        {renderChildren()}
+      </MenuContext.Provider>
+    </ul>
+  )
+}
+
+Menu.defaultProps = {
+  defaultIndex: '0',
+  mode: 'horizontal',
+  defaultOpenSubMenus: [],
+}
+
+export default Menu
