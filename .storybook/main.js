f576bbf141285e833a4e2edfcd1ade4113daa072
Joyce Wu <jianjoycewu@google.com>
2021-03-03 20:16:11 -0800

    reconfigure storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,22 @@
 module.exports = {
-  stories: ['../src/**/*.stories.@(ts|tsx)'],
+  stories: ['../src/**/*.stories.tsx', '../src/styles/index.scss'],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
     '@storybook/preset-create-react-app',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
   ],
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => {
+        if (prop.parent) {
+          return !prop.parent.fileName.includes('node_modules')
+        }
+        return true
+      },
+    },
+  },
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,30 @@
+import { configure, addDecorator, addParameters } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
+import React from 'react'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 import '../src/styles/index.scss'
 
-export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
+library.add(fas)
+
+const wrapperStyle = {
+  padding: '20px 40px',
+  width: '800px',
 }
+
+const storyWrapper = (stroyFn) => <div style={wrapperStyle}>{stroyFn()}</div>
+
+addDecorator(storyWrapper)
+addDecorator(withInfo)
+addParameters({ info: { inline: true, header: false } })
+
+const loaderFn = () => {
+  return [
+    require('../src/components/Button/button.stories.tsx'),
+    require('../src/components/Menu/menu.stories.tsx'),
+    require('../src/components/Input/input.stories.tsx'),
+  ]
+}
+
+// automatically import all files ending in *.stories.js
+configure(loaderFn, module)
