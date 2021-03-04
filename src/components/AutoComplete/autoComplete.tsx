44d97537968a820c7fa2fac6ca1164ffecb6812d
Joyce Wu <jianjoycewu@google.com>
2021-03-04 00:38:17 -0800

    can send asynchronous request to fetch data

--- src/components/AutoComplete/autoComplete.tsx
+++ src/components/AutoComplete/autoComplete.tsx
@@ -1,34 +1,64 @@
-import React, { FC, useState, ChangeEvent } from 'react'
+import React, { FC, useState, ChangeEvent, ReactElement } from 'react'
 import Input, { InputProps } from '../Input/input'
+import Icon from '../Icon/icon'
 
-export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
-  fetchSuggestions: (str: string) => string[]
-  onSelect?: (item: string) => void
+interface DataSourceObject {
+  value: string
 }
 
+export type DataSourceType<T = {}> = T & DataSourceObject
+
+export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
+  fetchSuggestions: (
+    str: string
+  ) => DataSourceType[] | Promise<DataSourceType[]>
+  onSelect?: (item: DataSourceType) => void
+  renderOption?: (item: DataSourceType) => ReactElement
+}
 export const AutoComplete: FC<AutoCompleteProps> = (props) => {
-  const { fetchSuggestions, onSelect, value, ...restProps } = props
+  const {
+    fetchSuggestions,
+    onSelect,
+    value,
+    renderOption,
+    ...restProps
+  } = props
 
   const [inputValue, setInputValue] = useState(value)
-  const [suggestions, setSuggestions] = useState<string[]>([])
+  const [suggestions, setSuggestions] = useState<DataSourceType[]>([]) // 用来展示下拉框的值
+  const [loading, setLoading] = useState(false)
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value.trim()
+    const value = e.target.value.trim() // 去掉空格
     setInputValue(value)
+
+    // 如果value存在，那么就要更新下拉菜单的选项
     if (value) {
       const results = fetchSuggestions(value)
-      setSuggestions(results)
+      setLoading(true)
+      if (results instanceof Promise) {
+        results.then((data) => {
+          setLoading(false)
+          setSuggestions(data)
+        })
+      } else {
+        setSuggestions(results)
+      }
     } else {
-      setSuggestions([])
+      setSuggestions([]) // 如果没有返回，那么下拉菜单变成空
     }
   }
 
-  const handleSelect = (item: string) => {
-    setInputValue(item)
+  const handleSelect = (item: DataSourceType) => {
+    setInputValue(item.value)
     setSuggestions([])
     if (onSelect) {
       onSelect(item)
     }
+  }
+
+  const renderTemplate = (item: DataSourceType) => {
+    return renderOption ? renderOption(item) : item.value
   }
 
   const generateDropdown = () => {
@@ -37,7 +67,7 @@ export const AutoComplete: FC<AutoCompleteProps> = (props) => {
         {suggestions.map((item, index) => {
           return (
             <li key={index} onClick={() => handleSelect(item)}>
-              {item}
+              {renderTemplate(item)}
             </li>
           )
         })}
@@ -48,6 +78,11 @@ export const AutoComplete: FC<AutoCompleteProps> = (props) => {
   return (
     <div className="joyful-auto-complete">
       <Input value={inputValue} onChange={handleChange} {...restProps} />
+      {loading && (
+        <ul>
+          <Icon icon="spinner" spin></Icon>
+        </ul>
+      )}
       {suggestions.length > 0 && generateDropdown()}
     </div>
   )
